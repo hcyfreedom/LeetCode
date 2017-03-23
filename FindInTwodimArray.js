90dfb067a63edca1e47c57e54dae4d4a6d548268
hcyfreedom <956885844@qq.com>
2017-03-23 23:38:37 +0800

    二维数组查找

---
+++ FindInTwodimArray.js
@@ -0,0 +1,27 @@
+
+function findTarget(array,target) {
+    var len = array.length;
+    var i = len - 1;
+    var j = 0;
+
+    while(i >= 0 && array[i][j]){
+        if (target > array[i][j]){
+            j ++;
+        }else if (target < array[i][j]){
+            i --;
+        }else {
+            console.log('目标元素在第' + i + '行，' + '第' +j + '列');
+            // console.log(`目标元素处于第${i}行，第${j}列`);
+            return true;
+        }
+    }
+
+    // console.log(`没有哦~`);
+    return false;
+}
+// var arr = [[0,1,2,3,4],[1,2,3,4,5],[2,3,4,5,6],[3,4,5,6,7],[4,5,6,7,8]];
+// var arr = [[1,3,5,7,9],[2,8,11,19,30]]
+var arr=[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]];
+
+console.log(arr);
+findTarget(arr,5);
